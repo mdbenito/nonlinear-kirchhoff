8de64b6d127ceb1fdd5fd9dc1258040105c0f949
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-06-07 17:29:57 +0000

    Progress in IsometryConstraint. Sparsity patterns and update.

--- IsometryConstraint.cpp
+++ IsometryConstraint.cpp
@@ -8,39 +8,146 @@ namespace dolfin {
 
   IsometryConstraint::IsometryConstraint(const FunctionSpace& W)
     : _v2d(vertex_to_dof_map(W)),
-      B(std::make_shared<Matrix>()), Bt(std::make_shared<Matrix>())
+      _B(std::make_shared<Matrix>()), _Bt(std::make_shared<Matrix>())
   {
-    // Initialise B
-    // ...
-
     const Mesh& mesh = *(W.mesh());
 
+    // TODO Check that W is a DKT vector space with 3 components
+    if (W.dim() != 9*mesh.size_global(0))
+    {
+      dolfin_error("IsometryConstraint.cpp",
+                   "initialise discrete isometry constraint",
+                   "function space has wrong dimension");
+    }
+    if (W.num_sub_spaces() != 3)
+    {
+      dolfin_error("IsometryConstraint.cpp",
+                   "initialise discrete isometry constraint",
+                   "function space has wrong number of components");
+    }
+      
     // What's the difference with vertex_to_dofmap() ??
     // const auto& v2d = W.dofmap()->dofs(mesh, 0);
 
-    auto tensor_layout = B->factory().create_layout(2);
-    dolfin_assert(tensor_layout);  // what for?
+    {
+      auto tensor_layout = _B->factory().create_layout(2);  // 2 is the rank
+      dolfin_assert(tensor_layout);  // what for?
 
-    std::vector<std::shared_ptr<const IndexMap> > index_maps(2);
-    index_maps.push(W.dofmap()->index_map());
-    index_maps.push();
+      // FIXME: is this ok? every process should own a 4xN block
+      auto row_index_map = std::make_shared<IndexMap>(mesh.mpi_comm(), 4, 1);
+      row_index_map.set_local_to_global(WHAT HERE);
 
-    auto local_range = W.dofmap()->ownership_range();
-
-    tensor_layout->init(mesh.mpi_comm(), index_maps,
-                        TensorLayout::Ghosts::UNGHOSTED);
-
-    mesh.init(1, 0);  // Initialize edge -> vertex connections
-  }
-  
-  void IsometryConstraint::update(const Function& Y)
-  {
-    double dofs[3];
     
-    for (auto::size_t vid = 0; vid < _num_vertices; ++vid) {
-      dofs[0] = _v2d[vid]; dofs[1] = _v2d[vid+1]; dofs[2] = _v2d[vid+2];
-      // do stuff
-      // ...
+      std::vector<std::shared_ptr<const IndexMap> > index_maps
+      { row_index_map, W.dofmap()->index_map() };
+
+      auto local_column_range = W.dofmap()->ownership_range();
+
+      tensor_layout->init(mesh.mpi_comm(), index_maps,
+                          TensorLayout::Ghosts::UNGHOSTED);
+
+      SparsityPattern& pattern = *tensor_layout->sparsity_pattern();
+      pattern.init(mesh.mpi_comm(), index_maps);
+    
+      // _Build sparsity pattern
+      if (tensor_layout->sparsity_pattern())
+      {
+        std::size_t dofs[3];  // in order: point eval, dx, dy
+        for (VertexIterator v(mesh); !edge.end(); ++edge)
+        {
+          for (int sub = 0; sub < 3; ++sub)  // iterate over the 3 subspaces
+          {
+            dofs[0] = _v2d[9*v.index()+3*i];
+            dofs[1] = _v2d[9*v.index()+3*i+1];
+            dofs[2] = _v2d[9*v.index()+3*i+2];
+        
+            pattern.insert_global(0, dofs[1]);
+            pattern.insert_global(1, dofs[1]);
+            pattern.insert_global(1, dofs[2]);
+            pattern.insert_global(2, dofs[1]);
+            pattern.insert_global(2, dofs[2]);
+            pattern.insert_global(3, dofs[2]);
+          }
+        }
+        pattern.apply();
+      }
+      _B->init(*tensor_layout); 
+    }
+
+    // This is me being lazy and sloppy...
+    
+    {
+      auto tensor_layout = _Bt->factory().create_layout(2);  // 2 is the rank
+      dolfin_assert(tensor_layout);  // what for?
+
+      // FIXME: is this ok? every process should own a Nx4 block
+      auto col_index_map = std::make_shared<IndexMap>(mesh.mpi_comm(), 4, 1);
+      col_index_map.set_local_to_global(WHAT HERE);
+
+    
+      std::vector<std::shared_ptr<const IndexMap> > index_maps
+      { W.dofmap()->index_map(), col_index_map };
+
+      auto local_row_range = W.dofmap()->ownership_range();
+
+      tensor_layout->init(mesh.mpi_comm(), index_maps,
+                          TensorLayout::Ghosts::UNGHOSTED);
+
+      SparsityPattern& pattern = *tensor_layout->sparsity_pattern();
+      pattern.init(mesh.mpi_comm(), index_maps);
+    
+      // _Build sparsity pattern
+      if (tensor_layout->sparsity_pattern())
+      {
+        std::size_t dofs[3];
+        for (VertexIterator v(mesh); !edge.end(); ++edge)
+        {
+          for (int sub = 0; sub < 3; ++sub)  // iterate over the 3 subspaces
+          {
+            dofs[0] = _v2d[9*v.index()+3*i];
+            dofs[1] = _v2d[9*v.index()+3*i+1];
+            dofs[2] = _v2d[9*v.index()+3*i+2];
+
+            pattern.insert_global(dofs[1], 0);
+            pattern.insert_global(dofs[1], 1);
+            pattern.insert_global(dofs[1], 2);
+            pattern.insert_global(dofs[2], 1);
+            pattern.insert_global(dofs[2], 2);
+            pattern.insert_global(dofs[2], 3);
+          }
+        }
+        pattern.apply();
+      }
+      _Bt->init(*tensor_layout);
     }
   }
+  
+  void
+  IsometryConstraint::update(const Function& Y)
+  {
+    std::size_t dofs[3];
+    std::size_t rows[4] = {0, 1, 2, 3};
+    double values[4*3] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+    
+    auto yy = Y.vector();
+    assert(yy->local_range() == _B.local_range());
+    
+    for (VertexIterator v(mesh); !edge.end(); ++edge)
+    {
+      dofs[0] = _v2d[v.index()];
+      dofs[1] = _v2d[v.index()+1];
+      dofs[2] = _v2d[v.index()+2];
+
+      // Copy the values of Y into the 4x3 chunk:
+      /* 0 */     values[1] = 2*yy[dofs[1]];      /* 0 */
+      /* 0 */     values[4] =   yy[dofs[2]];      values[5] =   yy[dofs[1]];
+      /* 0 */     values[7] =   yy[dofs[2]];      values[8] =   yy[dofs[1]];
+      /* 0 */     /* 0 */                        values[11] = 2*yy[dofs[2]];
+
+      B->set(values, 4, rows, 3, dofs);  // set() uses global indices
+    }
+
+    B->apply("insert");
+  }
+  
 }
--- IsometryConstraint.h
+++ IsometryConstraint.h
@@ -8,17 +8,17 @@ namespace dolfin {
     std::vector<la_index> _v2d;
     /// The Constraint matrix. This is going to be used in a BlockMatrix
     /// which requires shared ownership via a shared_ptr.
-    std::shared_ptr<Matrix> B;
+    std::shared_ptr<Matrix> _B;
     /// A copy of the transposed Constraint matrix. Stored for speed.
     /// TODO: test whether this is indeed faster
-    std::shared_ptr<Matrix> Bt;
+    std::shared_ptr<Matrix> _Bt;
     
   public:
     IsometryConstraint(const FunctionSpace& W);
     
     void update(const Function& Y);
-    std::shared_ptr<const Matrix> get() { return B; }
-    std::shared_ptr<const Matrix> get_transposed() { return Bt; }
+    std::shared_ptr<const Matrix> get() { return _B; }
+    std::shared_ptr<const Matrix> get_transposed() { return _Bt; }
   };
 }
 
