406fd26587472754e14d65dd69dce316915798f0
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-06-10 20:17:21 +0000

    Add some handy dir-locals

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil .
+      ((compile-command . "cd /home/fenics/local/src/nonlinear-kirchhoff/build && make -k -j4")
+       (cmake-ide-build-dir . "/docker:fenics@fenics-dev:/home/fenics/local/src/nonlinear-kirchhoff/build")
+       (c-basic-offset . 2)
+       (c-default-style . "linux"))))
+
