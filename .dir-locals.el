1686db9ab07ec27d3e893a733e2deff58af1af56
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-06-11 09:58:19 +0000

    Fix compile-command

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,7 +2,7 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil .
-      ((compile-command . "cd /home/fenics/local/src/nonlinear-kirchhoff/build && make -k -j4")
+      ((compile-command . "cd build && make -k -j4")
        (cmake-ide-build-dir . "/docker:fenics@fenics-dev:/home/fenics/local/src/nonlinear-kirchhoff/build")
        (c-basic-offset . 2)
        (c-default-style . "linux"))))
