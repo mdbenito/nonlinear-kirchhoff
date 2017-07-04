fa8400be8bc100089f2894005ccc1b9b7bec2f10
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-07-04 11:24:19 +0000

    Fix dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,9 +1,8 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((nil .
-      ((compile-command . "cd build && make -k -j4")
-       (cmake-ide-build-dir . "/home/fenics/local/src/nonlinear-kirchhoff/build")
-       (c-basic-offset . 2)
-       (c-default-style . "linux"))))
-
+((nil . (
+         ;; Number of jobs (-j nn) should already be set with MAKEFLAGS in .bashrc
+         (compile-command . "cd build && make -k")
+         ;; Use a wrapper around cmake which sets FEniCS' environment variables
+         (cmake-ide-cmake-command . "/home/fenics/bin/fenics-cmake")
+         (cmake-ide-build-dir . "/home/fenics/local/src/nonlinear-kirchhoff/build")
+         (c-basic-offset . 2)
+         (c-default-style . "linux"))))
