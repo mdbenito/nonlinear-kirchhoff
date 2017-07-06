b0df77e4eed6040ff32840728b648032bcfb85a8
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-07-06 11:46:28 +0000

    Don't set no. of jobs for make since we are using MAKEFLAGS

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,5 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
 ((nil .
-      ((compile-command . "cd build && make -k -j4")
+      ((compile-command . "cd build && make -k")
        (cmake-ide-build-dir . "/home/fenics/local/src/nonlinear-kirchhoff/build")
        (c-basic-offset . 2)
        (c-default-style . "linux"))))
