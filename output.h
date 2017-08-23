9676f68860b681f368dd3644e126a926576b68b4
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-08-23 08:30:40 +0000

    Cosmetic.

--- output.h
+++ output.h
@@ -13,14 +13,19 @@ namespace NLK {
 
   extern int DEBUG;
 
-  // HACK: I don't know how to use the common interface of
+  // HACK(S): I don't know how to use the common interface of
   // GenericTensor and I don't have the time to learn it
+
+  /*! Writes a tensor in format understandable by numpy.loadtxt() */
   void dump_full_tensor(const dolfin::GenericMatrix& A, int precision=14,
                         const std::string& name="", bool asfile=true);
+  /*! Writes a vector in format understandable by numpy.loadtxt() */  
   void dump_full_tensor(const dolfin::GenericVector& A, int precision=14,
                         const std::string& name="", bool asfile=true);
+  /*! Writes a matrix in format understandable by numpy.loadtxt() */
   void dump_raw_matrix(const double* A, int m, int n, int precision=14,
                        const std::string& name="", bool asfile=true);
+  /*! Writes a vector in format understandable by numpy.loadtxt() */  
   void dump_raw_matrix(const std::vector<double>& A, int m, int n,
                        int precision=14,
                        const std::string& name="", bool asfile=true);
