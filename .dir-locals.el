accf85d9bf9a5bd0f3714aaef9ec9d4d94fa26ae
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-08-25 10:52:10 +0000

    Compile with 2 jobs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,6 @@
 ((nil . (
          ;; Number of jobs (-j nn) should already be set with MAKEFLAGS in .bashrc
-         (compile-command . "cd build && make -k")
+         (compile-command . "cd build && make -j 2 -k")
          ;; Use a wrapper around cmake which sets FEniCS' environment variables
          (cmake-ide-cmake-command . "/home/fenics/bin/fenics-cmake")
          (cmake-ide-build-dir . "/home/fenics/local/src/nonlinear-kirchhoff/build")
