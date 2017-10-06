85f624702c1e90c99055b7ac7d1ac0426b98bd55
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-10-06 11:55:52 +0200

    Compile using more cores by default, plus fix cmake-ide defaults

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,8 +1,8 @@
 ((nil . (
          ;; Number of jobs (-j nn) should already be set with MAKEFLAGS in .bashrc
-         (compile-command . "cd build && make -j 2 -k")
+         (compile-command . "cd build && make -j 12 -k")
          ;; Use a wrapper around cmake which sets FEniCS' environment variables
          (cmake-ide-cmake-command . "/home/fenics/bin/fenics-cmake")
-         (cmake-ide-build-dir . "/home/fenics/local/src/nonlinear-kirchhoff/build")
+         (cmake-ide-build-dir . "build")
          (c-basic-offset . 2)
          (c-default-style . "linux"))))
